9eb61db5e646f18cd3b3acd6b18ca445fb6e5737
eunsu <eunsu2415@gmail.com>
2021-07-08 12:24:59 +0900

    add code

--- Input512.java
+++ Input512.java
@@ -3,7 +3,10 @@ package input;
 public class Input512 {
 
 	public static void main(String[] args) {
-		
-	}
 
+		int weight = 49;
+		double ratio = 0.2683;
+
+		System.out.println(weight + " * " + ratio + "00 = " + weight * ratio + "00");
+	}
 }
