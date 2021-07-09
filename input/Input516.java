be6a82ccc287416f856279413c6599800514adaa
Eunsu kim <69671515+eunsu621@users.noreply.github.com>
2021-07-09 17:49:16 +0900

    Rename Input516.java to input/Input516.java

--- input/Input516.java
+++ input/Input516.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Input516 {
 	
-	/* ���� : �Է� - �ڰ�����8
-	 * �Ǽ� 2���� �� ���� ���ڸ� �Է� �޾� ����ϵ� �Ǽ��� �ݿø��Ͽ� �Ҽ� ��°�ڸ����� ����ϴ� ���α׷����ۼ��Ͻÿ�.
-	 * (C, C++, Java �� ��� �Ǽ��� "double"�� �����ϼ���.)
+	/* 문제 : 입력 - 자가진단8
+	 * 실수 2개와 한 개의 문자를 입력 받아 출력하되 실수는 반올림하여 소수 둘째자리까지 출력하는 프로그램을작성하시오.
+	 * (C, C++, Java 의 경우 실수는 "double"로 선언하세요.)
 	 */
 	
 	 public static void main(String[] args) {
