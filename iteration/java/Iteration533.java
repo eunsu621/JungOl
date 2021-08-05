c6e0641798b867efdf2e7be4f9594b74148e4f42
Eunsu kim <69671515+eunsu621@users.noreply.github.com>
2021-08-05 09:21:19 +0900

    정올 Language_Coder 반복제어문3 553

---
+++ iteration/java/Iteration533.java
@@ -0,0 +1,31 @@
+package iteration;
+
+import java.util.Scanner;
+
+public class Iteration533 {
+	
+	/*
+	 * 자연수 n( 3 <= n <= 10) 을 입력받아 다음과 같이 영문자를 출력하는 프로그램을 작성하시오.
+	 */
+	
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+
+		char aString = 65;
+		
+		for (int i = 1; i <= n; i++) {
+			
+			for (int j = i; j <= n; j++) {
+				
+				String str = String.valueOf(aString);
+				System.out.print(str);
+				
+				aString++;
+			}
+			System.out.println();
+		}
+	}
+}
