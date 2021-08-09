71bd046c34e5c384c8abbf95488c3f406d7cea15
Eunsu kim <69671515+eunsu621@users.noreply.github.com>
2021-08-09 09:14:28 +0900

    Add files via upload
    정올 Language_Coder 반복제어문3 142

---
+++ iteration/java/Iteration142.java
@@ -0,0 +1,35 @@
+package iteration;
+
+import java.util.Scanner;
+
+public class Iteration142 {
+	
+	/* 문제 : 반복제어문2 - 형성평가
+	 * 자연수 n을 입력받아 "출력 예"와 같이 출력되는 프로그램을 작성하시오.
+	 * 주의! '*'과 '*'사이에 공백이 없고 줄사이에도 빈줄이 없다.
+	 */
+	
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+
+		for (int i = 1; i <= n * 2; i++) {
+
+			if (i < n) {
+				
+				for (int j = 1; j <= i; j++) {
+					System.out.print("*");
+				}
+			} else {
+				
+				for (int j = i; j < n * 2; j++) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
