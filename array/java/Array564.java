d4c09dc80442e71d167b2085fc0879ea5732ec05
Eunsu kim <69671515+eunsu621@users.noreply.github.com>
2021-08-28 10:22:11 +0900

    정올 Language_Coder 배열1 564

---
+++ array/java/Array564.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.Scanner;
+
+public class Array564 {
+
+	/*
+	 * 문제 : 배열1 - 형성평가 10개의 문자를 입력받아 마지막으로 입력받은 문자부터 첫 번째 입력받은 문자까지 차례로 출력하는 프로그램을
+	 * 작성하시오.
+	 */
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		String[] strArr = new String[10];
+
+		for (int i = 0; i < strArr.length; i++) {
+			String inputStr = scan.next();
+			strArr[i] = inputStr;
+		}
+
+		for (int i = strArr.length - 1; i >= 0; i--) {
+			System.out.print(strArr[i] + " ");
+		}
+	}
+}
