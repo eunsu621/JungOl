c69b13ac439dc8d20ddeaab5d29308bc226446a6
Eunsu kim <69671515+eunsu621@users.noreply.github.com>
2021-09-02 08:55:24 +0900

    정올 Language_Coder 배열1 155

---
+++ array/java/Array155.java
@@ -0,0 +1,37 @@
+package array;
+
+import java.util.Scanner;
+
+public class Array155 {
+
+	/* 문제 : 배열1 - 형성평가5
+	 * 6개의 문자배열을 만들고 {'J', 'U', 'N', 'G', 'O', 'L'} 으로 초기화 한 후 문자 한
+	 * 개를 입력받아 배열에서의 위치를 출력하는 프로그램을 작성하시오.
+	 * 첫 번째 위치는 0번이며 배열에 없는 문자가 입력되면 "none" 라는 메시지를 출력하고 끝내는 프로그램을 작성하시오.
+	 */
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		char[] charArr = { 'J', 'U', 'N', 'G', 'O', 'L' };
+
+		String target = scan.next();
+		String none = "";
+
+		for (int i = 0; i < charArr.length; i++) {
+
+			if (Character.toString(charArr[i]).matches(target)) {
+				System.out.println(i);
+				none = "isValue";
+				break;
+			} else {
+				none = "none";
+			}
+		}
+		
+		if (none.equals("none")) {
+			System.out.println(none);
+		}
+	}
+}
